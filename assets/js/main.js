d6c1c16b4eae5a99811d5da4a4ff1b990373fb6f
daniltop3309 <114923511+daniltop3309@users.noreply.github.com>
2022-10-24 15:23:08 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,136 @@
+//let a = 1, b = 1;
+//let c = ++ a;
+//let d = b ++;
+//console.log(a)
+//console.log(b)
+//console.log(c)
+//console.log(d)
+
+
+//let a = 2;
+//let x = 1 + (a *= 2);
+//console.log(a)
+//console.log(x)
+
+
+//"" + 1 + 0 //1
+//"" - 1 + 0 //-1
+//true + false //1
+//6 / "3" //2
+//"2" * "3" //6
+//4 + 5 + "px" //9px
+//"$" + 4 + 5 //$45
+//"4" - 2 //2
+//"4px" - 2 //NaN
+//"  - 9  "+ 5 //-4
+//"  - 9  "- 5 //-14
+//null + 1 //1
+//undefined + 1 //NaN
+//" \t \n" - 2 //-2
+
+
+//let a = prompt("Первое число", 1);
+//let b = prompt("Второе число", 2);
+//alert(+a + +b)
+
+
+//let num = 3;
+//alert(num)
+
+
+//let a =10;
+//let b =2;
+//alert(a + b,)
+//alert(a - b,)
+//alert(a / b,)
+//alert(a * b,)
+
+
+//let c = 15;
+//let d = 2;
+//let result = c + d;
+//alert(result)
+
+
+//let a = 10; 
+//let b = 2;
+//let c = 5;
+//alert(a + c + b)
+
+
+//let a = 17;
+//let b = 10;
+//let c = a - b;
+//let d = 7;
+//let result = c + d;
+//alert(result) 
+
+
+//let str = ("Привет Мир");
+//alert(str)
+
+
+//let str1 = "Привет,";
+//let str2 = " Мир!";
+//alert(str1 + str2)
+
+
+//let name = 'Даниль', str1 = "Привет, ", str2 = " !";
+//alert(str1 + name + str2) 
+
+
+//let age = 16;
+//let str1 ="Мне ", str2 = " лет!";
+//alert(str1 + age + str2)
+
+
+//let name = prompt("Ваше имя?", "");
+//alert(`Ваше имя ${name}`);
+
+
+//let num = prompt("Напишите число", "");
+//alert(`Ваше число в квадрате ${num**2}`);
+
+
+//let str = "abcde";
+//alert((str[0])+(str[2])+(str[4]час часми))
+
+
+//let num = "12345";
+//alert((num[0])*(num[1])*(num[2])*(num[3])*(num[4]))
+
+
+//let secondInhour = 60 * 60;
+//let secondInday = secondInhour * 24;
+//let secondInmon = secondInday * 30;
+//alert(secondInhour)
+//alert(secondInday)
+//alert(secondInmon)
+
+
+//let hour = 14;
+//let min = 33;
+//let sec = 54;
+//alert(hour+":"+min+":"+sec)
+
+//let date = new Date();
+//alert(date)
+
+
+//let num = 6;
+//alert(num**2)
+
+
+//var num = 47;
+//num += 7;
+//num -= 18;
+//num *= 10;
+//num /= 15;
+//alert(num);
+
+
+//var num = 10;
+//num ++;
+//num ++;
+//num --;
+//alert(num)
